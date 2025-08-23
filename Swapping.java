90e1e1be9d3055b265410f6f161934b04228450f
messilover9000 <sourjyabiswas03@gmail.com>
2025-08-23 14:02:42 +0530

    Swapping

---
+++ Swapping.java
@@ -0,0 +1,22 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        // Without using a temporary variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+
+    
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,7 +1,12 @@
-result = Math.abs(-5);
-        // result = Math.sqrt(9);
-        // result = Math.round(3.14);
-        // result = Math.ceil(3.14);
-        // result = Math.floor(3.99);
-        // result = Math.max(10, 20);
-        // result = Math.min(10, 20);
+public static void main(String[] args) {
+    //     int a = 5;
+    //     int b = 10;
+
+    //     System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
+
+    //     System.out.println("After swapping: a = " + a + ", b = " + b);
+    // }
